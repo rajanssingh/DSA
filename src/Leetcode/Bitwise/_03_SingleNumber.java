4ca90404c1a74e0b95c67393a9f78c94c0a5c9c5
Rajan Singh <rjnkumar628@gmail.com>
2024-05-29 23:06:49 +0530

    Bitwise Xor to find single number

---
+++ src/Leetcode/Bitwise/_03_SingleNumber.java
@@ -0,0 +1,14 @@
+package Leetcode.Bitwise;
+
+public class _03_SingleNumber {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int ans = nums[0];
+            for(int i=1;i<nums.length;i++){
+                ans = ans ^ nums[i];
+            }
+            return ans;
+        }
+    }
+}
+
